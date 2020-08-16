4aca23f81eceab9dade944ec26561289b65248e2
Jessical26 <42556066+Jessical26@users.noreply.github.com>
2020-08-16 00:53:29 -0700

    Add files via upload
    UI Design Ready

---
+++ Ace 2.0/Ace 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ace 2.0
+//
+//  Created by Jessica Liang on 8/16/20.
+//  Copyright © 2020 Jessica Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
